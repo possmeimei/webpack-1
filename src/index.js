855150504404c65287922ae31c2aef5a151987c0
zhangchen <383922689@qq.com>
2022-11-24 23:12:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import x from './x.js'
 console.log(x);
+import jpg from './assets/images/1.jpg'
+console.log(jpg);
+const div = document.getElementById('app')
+
+div.innerHTML =`
+    <img src="${jpg}" alt="暗黑破坏神"/>
+`
--- webpack.config-base.js
+++ webpack.config-base.js
@@ -15,6 +15,47 @@ module.exports = {
             }
         ),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.less$/i,
+                use: [
+                    // compiles Less to CSS
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ],
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    // compiles Less to CSS
+                    "style-loader",
+                    "css-loader",
+                    "stylus-loader",
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
 };
 
 
