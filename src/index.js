8c4f3a60b5ed169f6b43a83143ff4f172c1aee02
zhangchen <383922689@qq.com>
2022-11-24 23:26:29 +0800

    webpack 学习

--- src/index.js
+++ src/index.js
@@ -7,3 +7,14 @@ const div = document.getElementById('app')
 div.innerHTML =`
     <img src="${jpg}" alt="暗黑破坏神"/>
 `
+
+const button = document.createElement("button")
+button.innerText = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{})
+}
+div.appendChild(button)
