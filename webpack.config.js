4ac6d15737e8a9232b0c3dd73a6c2389899457e1
zhangchen <383922689@qq.com>
2022-11-24 20:49:33 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,4 +8,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
     },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: 'webpack 学习',
+            template: 'src/assets/test.html'
+        }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
+
+
